81d95023b8cd1e4f8e114ca9691ad052b2a137ef
Ubuntu <ubuntu@ip-172-31-16-204.ec2.internal>
2019-12-02 00:24:01 +0000

    mail format

--- mail_format.py
+++ mail_format.py
@@ -2,31 +2,48 @@
 
 # Created by: Teddy Sannan
 # Created on: November 2019
-# This program shows the volume of a cylinder
+# This program shows letter formatting
 
 
-def mailing(name1, address1, city1, province1, postal_code1):
-    print(name1)
-    print(address1)
-    print(city1, province1, postal_code1)
+def letter(name, address, city, province, postal_code, apartment_number=None):
+    # formats letter
+    if apartment_number is not None:
+        letter = "{0} \n{1} {2} \n{3} {4} {5}".format(name, apartment_number,
+                                                      address, city,
+                                                      province,
+                                                      postal_code)
+
+    letter = "{0} \n{1} \n{2} {3} {4}".format(name, address, city, province,
+                                              postal_code)
+
+    return letter
 
 
 def main():
+    # calls other functions
     while True:
-        name = input("Enter the name of the reciever: ")
-        address = input("Enter the address: ")
-        city = input("Enter the city: ")
-        province = input("Enter the province: ")
-        postal_code = input("Enter the postal code: ")
-        print()
 
-        try:
-            mailing(name, address, city, province, postal_code)
-        except ValueError:
-            print("Invalid Input")
-            continue
+        name = input("Enter your full name: ")
+        apartment = input("Do you live in an apartment?(y/n): ")
+        if apartment.upper() == "Y" or apartment.upper() == "YES":
+            apartment_number = input("What is your apartment number: ")
+        address = input("Enter your adress: ")
+        city = input("Enter your city: ")
+        province = input("Enter your province or territory \
+(short form): ")
+        postal_code = input("Enter your postal code: ")
+
+        if apartment == "y":
+            card = letter(name=name, address=address, city=city,
+                          province=province, postal_code=postal_code,
+                          apartment_number=apartment_number)
+
         else:
-            break
+            card = letter(name=name, address=address, city=city,
+                          province=province, postal_code=postal_code)
+        print("")
+        print(card)
+        break
 
 
 if __name__ == "__main__":
